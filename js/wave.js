8d9d255872686583c4086315086f2f720811fc40
ThinkingDobby <chiho1221@naver.com>
2022-12-16 15:56:56 +0900

    색상 세부 조정

---
+++ js/wave.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+  shininess: 20,
+  el: ".background",
+  color: "#0C0A09",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  waveSpeed: 0.1,
+  waveHeight: 5,
+  zoom: 1.8,
+});
