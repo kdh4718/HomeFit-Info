977ad077934bf6d8141481c77f58d2d518f5d550
inwoong <525inwung@gmail.com>
2022-12-17 16:00:09 +0900

    롤백

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -1,3 +1,23 @@
+function scrollTrigger(targetArea, typedclass) {
+  let isVisible = false;
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= Math.ceil($(targetArea).offset().top)) {
+      $(typedclass).typed({
+        strings: ["앱설명 어떤식으로 할지 정해야 해요."],
+        typeSpeed: 10,
+        backSpeed: 40,
+        backDelay: 200,
+        smartBackspace: true,
+        fadeOut: true,
+        cursorChar: "",
+        loop: false,
+      });
+      isVisible = true;
+    } else {
+    }
+  });
+}
+
 $(function () {
   let isVisible = false;
   $(window).scroll(function () {
@@ -10,7 +30,7 @@ $(function () {
         typeSpeed: 100,
         backSpeed: 1,
         backDelay: 200,
-        startDelay: 1300,
+        startDelay: 1500,
         smartBackspace: true,
         fadeOut: true,
         cursorChar: "",
