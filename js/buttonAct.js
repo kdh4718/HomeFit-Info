8bfdab6e8665a1281a641ac3bbb07dfec1186324
ThinkingDobby <chiho1221@naver.com>
2022-12-22 15:24:29 +0900

    메뉴 적용 중 임시저장

--- js/buttonAct.js
+++ js/buttonAct.js
@@ -1,5 +1,15 @@
+var count = 0;
+
 $(document).ready(function () {
   $(".hamburger").click(function () {
     $(this).toggleClass("is-active");
+
+    if (count == 0) {
+      $(".menu").fadeIn(1000);
+      count++;
+    } else {
+      $(".menu").fadeOut(1000);
+      count--;
+    }
   });
 });
